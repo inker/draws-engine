adb98a4aaf28c94963dcf07d0429a16c35e9a241
inker <anton.veselyev@gmail.com>
2022-08-23 22:15:59 +0100

    update pots

---
+++ tests/numbers.test.ts
@@ -0,0 +1,11 @@
+import { allPossibleGroups } from '../src'
+
+const numbers = range(32)
+const pots = chunk(numbers, 8)
+
+describe('numbers', () => {
+  it('should work correctly for every combo', () => {
+    const groups = []
+    allPossibleGroups(pots, )
+  })
+})
